f43b957fa79814004a4a80a2e7da9b580bcd5787
@luizcgr <luizcgr@gmail.com>
2024-09-23 18:01:09 -0300

    feat: validacoes vs acoplamento

--- src/domain/product/factory/product.validator.factory.ts
+++ src/domain/product/factory/product.validator.factory.ts
@@ -1,9 +1,9 @@
 import ValidatorInterface from "../../@shared/validator/validator.interface";
 import Product from "../entity/product";
-import { ProductValidator } from "../validator/product.validator";
+import { ProductYupValidator } from "../validator/product.yup.validator";
 
 export default class ProductValidatorFactory {
   static create(): ValidatorInterface<Product> {
-    return new ProductValidator();
+    return new ProductYupValidator();
   }
 }
